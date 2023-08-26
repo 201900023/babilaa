0084091c39f5ccdfa6c2ff7fe1df13baa5612376
dag-bag <virenderkumar23435@gmail.com>
2023-08-26 21:10:04 +0530

    Update post-sharing-modal.tsx

--- src/components/post-card/post-sharing-modal.tsx
+++ src/components/post-card/post-sharing-modal.tsx
@@ -1,10 +1,10 @@
-import { PostDetailsType } from '@/types/db';
-import { useState } from 'react';
-import PostInput from '@/components/post-input/post-input';
-import { useToggleUserShareMutation } from '@/hooks/mutation';
-import ModalWrapper from '../common/modal-wrapper';
-import PostThumbnail from '../post/post-thumbnail';
-import Button from '../common/button';
+import { PostDetailsType } from "@/types/db";
+import { useState } from "react";
+import PostInput from "@/components/post-input/post-input";
+import { useToggleUserShareMutation } from "@/hooks/mutation";
+import ModalWrapper from "../common/modal-wrapper";
+import PostThumbnail from "../post/post-thumbnail";
+import Button from "../common/button";
 
 interface PostSharingModalProps {
   closeSharingModal: () => void;
@@ -15,7 +15,7 @@ const PostSharingModal = ({
   closeSharingModal,
   sharedPost,
 }: PostSharingModalProps) => {
-  const [isQuoteShare, setIsQuoteShare] = useState(true);
+  const [isQuoteShare, setIsQuoteShare] = useState(false);
 
   const toggleUserShare = useToggleUserShareMutation(closeSharingModal);
 
@@ -27,7 +27,7 @@ const PostSharingModal = ({
 
   return (
     <ModalWrapper handleCloseModal={closeSharingModal} title="Share">
-      <div className="flex items-center mt-1">
+      {/* <div className="flex items-center mt-1">
         <span className="mr-3 text-sm font-medium text-gray-900 dark:text-gray-300">
           Quote Share
         </span>
@@ -45,7 +45,7 @@ const PostSharingModal = ({
           />
           <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-500 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500" />
         </label>
-      </div>
+      </div> */}
 
       {isQuoteShare ? (
         <div className="bg-primary-0 dark:bg-primary-dark-200 px-5 py-3 rounded-lg">
@@ -62,7 +62,7 @@ const PostSharingModal = ({
             className="ml-auto block"
             onClick={handleToggleUserShare}
           >
-            {sharedPost.sharedByMe ? 'Unshare' : 'Share'}
+            {sharedPost.sharedByMe ? "Unshare" : "Share"}
           </Button>
         </>
       )}
